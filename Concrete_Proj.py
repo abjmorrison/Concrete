0430145f644d39304fee54c42b994dad44871cd1
Alicia Morrison <aliciamorrison@Alicias-MacBook-Pro.local>
2019-02-20 10:44:37 -0600

    20190220 - EDA

--- Concrete_Proj.py
+++ Concrete_Proj.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 #%%
+# EDA
+    # 
+#%%
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
@@ -7,6 +10,7 @@ import seaborn
 import seaborn as sns
 sns.set(color_codes=True)
 from scipy import stats
+import numpy as np
 #%%
 conc = pd.read_csv("Concrete_Data_Yeh.csv")
 
@@ -16,7 +20,12 @@ cols.pop()
 #['cement', 'slag', 'flyash', 'water', 'superplasticizer', 'coarseaggregate', 'fineaggregate', 'age', 'csMPa']
 
 #%%
+conc.dtypes
 
+#%%
+conc.isnull().sum()
+
+#%%
 desc = conc.describe()
 
 #%%
@@ -36,6 +45,36 @@ for f, a in cols:
 
 fig.set_size_inches(12, 12)
 fig.savefig('mats_distro.png')
+
+#%%
+#corr = conc.iloc[:,:-1].corr() 
+corr = conc.corr()
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+f, ax = plt.subplots(figsize=(11, 9))
+
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+sns.heatmap(corr, mask=mask, cmap=cmap, annot=True, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
+#%%
+sns.scatterplot( x=conc['age'], y=conc['csMPa'])
+
+#%%
+y=conc['csMPa']
+sns.scatterplot( x=conc['cement'], y=y)
+
+#%%
+sns.scatterplot( x=conc['superplasticizer'],y=y)
 #%%
 
+sns.scatterplot( x=conc['age'],y=y, label='age')
+sns.scatterplot( x=conc['cement'], y=y, label='cement')
+sns.scatterplot( x=conc['superplasticizer'], y=y, label='superplasticizer')
+
+plt.xlabel('Independent Features')
+plt.title('Relationship of highly correlated features with csMPA')
+fig.legend()
+plt.show(fig)
+#%%
 
